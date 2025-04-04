aa69c38bcd3791cf1e4fb49a992a17b8be0f2f74
Ivan Dutković <idutkovic@tvz.hr>
2025-04-04 02:09:54 +0200

    Update project files

--- Lab1/sketch.ino
+++ Lab1/sketch.ino
@@ -18,6 +18,12 @@
 #define TIMER_DELAY        1000
 #define ALARM_DISTANCE     100
 
+#define LOGIC_ANALYZER_PIN0 14 
+#define LOGIC_ANALYZER_PIN1 15 
+#define LOGIC_ANALYZER_PIN2 16 
+#define LOGIC_ANALYZER_TIMER 17
+#define LOGIC_ANALYZER_SENSOR 18 
+
 volatile bool     intFlag[3] = {0};
 volatile unsigned long lastInterruptTime[3] = {0}, 
                        lastTimerTime = 0;
@@ -37,6 +43,12 @@ void setup(){
 
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
+
+    pinMode(LOGIC_ANALYZER_PIN0, OUTPUT);
+    pinMode(LOGIC_ANALYZER_PIN1, OUTPUT);
+    pinMode(LOGIC_ANALYZER_PIN2, OUTPUT);
+    pinMode(LOGIC_ANALYZER_TIMER, OUTPUT);
+    pinMode(LOGIC_ANALYZER_SENSOR, OUTPUT);
 
     attachInterrupt(digitalPinToInterrupt(BUTTON0), ISR_INT0, FALLING);
     attachInterrupt(digitalPinToInterrupt(BUTTON1), ISR_INT1, FALLING);
@@ -63,8 +75,10 @@ void loop(){
 }
 
 ISR(TIMER1_COMPA_vect){
+  digitalWrite(LOGIC_ANALYZER_TIMER, HIGH);
     timerFlag = true;
     lastTimerTime = millis();
+    digitalWrite(LOGIC_ANALYZER_TIMER, LOW);
 }
 
 void handleTimerInterrupt(){
@@ -75,9 +89,21 @@ void handleTimerInterrupt(){
     timerFlag = false;
 }
 
-void ISR_INT0(){ handleInterrupt(0, LED_INT0, "INT0 (HIGH priority) triggered"); }
-void ISR_INT1(){ handleInterrupt(1, LED_INT1, "INT1 (MEDIUM priority) triggered"); }
-void ISR_INT2(){ handleInterrupt(2, LED_INT2, "INT2 (LOW priority) triggered"); }
+void ISR_INT0(){
+  digitalWrite(LOGIC_ANALYZER_PIN0, HIGH);
+  handleInterrupt(0, LED_INT0, "INT0 (HIGH priority) triggered");
+  digitalWrite(LOGIC_ANALYZER_PIN0, LOW);
+}
+void ISR_INT1(){ 
+  digitalWrite(LOGIC_ANALYZER_PIN1, HIGH);
+  handleInterrupt(1, LED_INT1, "INT1 (MEDIUM priority) triggered");
+  digitalWrite(LOGIC_ANALYZER_PIN1, LOW);
+}
+void ISR_INT2(){
+  digitalWrite(LOGIC_ANALYZER_PIN2, HIGH);
+  handleInterrupt(2, LED_INT2, "INT2 (LOW priority) triggered");
+  digitalWrite(LOGIC_ANALYZER_PIN2, HIGH);
+}
 
 void handleInterrupt(int i, int led, const char* msg){
     if((millis() - lastInterruptTime[i]) < DEBOUNCE_DELAY) return;
@@ -118,11 +144,13 @@ float measureDistance(){
 void triggerDistanceAlert() {
     static unsigned long prev = 0;
     if (distanceAlert) {
+      digitalWrite(LOGIC_ANALYZER_SENSOR, HIGH);
         if (millis() - prev >= 200) {
             prev = millis();
             digitalWrite(LED_Sensor, !digitalRead(LED_Sensor));
         }
         Serial.println("ALARM: Predmet preblizu (<100cm, LOWEST PRIORITY)!");
+        digitalWrite(LOGIC_ANALYZER_SENSOR, LOW);
     } else {
         digitalWrite(LED_Sensor, LOW);  // Turn off LED when distance is safe
     }
